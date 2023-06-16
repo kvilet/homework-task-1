6ce67428bf6f04a4476b671403512392fb340fcf
Janna <jannasheyanova@gmail.com>
2023-06-16 17:05:09 +0300

    Resolved task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
 
+    public static void main(String[] args) {
+        int amount = 11_430;
+        int mile = 20;
+        int bonus= amount / mile;
+        System.out.println(bonus);
 
+
+    }
 
 }
